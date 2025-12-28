fb6b2aa92458183d1dd339a8e908a855ac0172bf
F-sedighi <sedighifardin@gmail.com>
2025-12-28 20:48:21 +0330

    Modify

--- mysite2_copy/blog/views.py
+++ mysite2_copy/blog/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
-from datetime import datetime
-
+from django.utils import timezone
 
 # Create your views here.
 def blog_view(request):
-    posts = Post.objects.filter(status = 1,published_date__lte = datetime.now())
+    posts = Post.objects.filter(status = 1,published_date__lte = timezone.now())
     context = {'posts': posts}
     return render(request,'blog/blog-home.html', context)
 
--- mysite2_copy/mysite2_copy/settings.py
+++ mysite2_copy/mysite2_copy/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'mysite2_copy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mysite2_copy',
-        'USER': 'fardin',
-        'PASSWORD': '1370fard*',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
